2e74516217eba35462e174186378c387d9a33354
orley333@gmail.com <orley333@gmail.com@8e16a1f9-48f4-33b4-0e1b-ee972f93d831>
2014-10-17 04:33:32 +0000

    Se confirma: se estan haciendo los ajustes para los registros de las personas

---
+++ Entidades/es_tercerosDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class es_tercerosDto
+    {
+        public string terceroid { get; set; }
+        public string nombre { get; set; }
+        public string tipodoc { get; set; }
+        public string tipoper { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+        public int indice_id { get; set; }
+    }
+}
