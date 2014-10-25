7a3c5963918156f42e205a288f7b94128dab49fb
orley333@gmail.com <orley333@gmail.com@8e16a1f9-48f4-33b4-0e1b-ee972f93d831>
2014-10-25 21:22:28 +0000

    Se confirma: ajustes de manejo de la aplicacion...

--- trunk/Program.cs
+++ trunk/Program.cs
@@ -14,7 +14,7 @@ namespace FingerprintNetSample
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new PruebaReporte());
         }
     }
 }
